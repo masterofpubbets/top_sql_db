ac6e227033c427ddd3c41484c591bd0a1051d971
Eslam Aly <70063355+masterofpubbets@users.noreply.github.com>
2021-01-04 16:41:35 +0400

    Create P6 Summary Procedure

---
+++ sewa/sp_p6Summary.sql
@@ -0,0 +1,41 @@
+CREATE PROC sp_p6Summary
+AS
+DECLARE @dte DATE
+SELECT @dte = tblOptions.cutoffDate FROM tblOptions WHERE id = 1
+
+SELECT
+tblActids.area AS Unit,tblActids.family AS Family,tblActids.act_name AS [Activity],tblActids.subcon As Subcontractor
+,itemsProduction.*
+
+FROM (
+--Cables Pulling
+SELECT 
+pulling_actid AS [ActID]
+,SUM(tblCables.design_length) AS [Scope]
+,SUM(CASE WHEN tblCables.pulled_date <= @dte THEN design_length ELSE 0 END) AS [Cummlative]
+,SUM(CASE WHEN pulled_date BETWEEN DATEADD(DAY,-6,@dte) AND @dte THEN design_length ELSE 0 END) As [Weekly]
+FROM tblCables
+GROUP BY tblCables.pulling_actid
+UNION ALL
+--Cables Pulling
+SELECT 
+con_actid AS [ActID]
+,COUNT(tblCables.tag) * 2 AS [Scope]
+,SUM(CASE WHEN tblCables.con_from_date <= @dte THEN 1 ELSE 0 END) + SUM(CASE WHEN tblCables.con_to_date <= @dte THEN 1 ELSE 0 END)
+AS [Cummlative]
+,SUM(CASE WHEN con_from_date BETWEEN DATEADD(DAY,-6,@dte) AND @dte THEN 1 ELSE 0 END) + SUM(CASE WHEN con_to_date BETWEEN DATEADD(DAY,-6,@dte) AND @dte THEN 1 ELSE 0 END)
+As [Weekly]
+FROM tblCables
+GROUP BY tblCables.con_actid
+UNION ALL
+--Instrument Installation
+SELECT 
+installation_actid AS [ActID]
+,COUNT(tblInstruments.tag) AS [Scope]
+,SUM(CASE WHEN tblInstruments.installed_date <= @dte THEN 1 ELSE 0 END) AS [Cummlative]
+,SUM(CASE WHEN installed_date BETWEEN DATEADD(DAY,-6,@dte) AND @dte THEN 1 ELSE 0 END) As [Weekly]
+FROM tblInstruments
+WHERE main_device = 1 AND Installation_scope LIKE 'TR%'
+GROUP BY tblInstruments.installation_actid
+) AS itemsProduction
+LEFT JOIN tblActids ON itemsProduction.ActID = tblActids.actid
